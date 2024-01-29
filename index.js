77ae1e55db95c6385176e1ec5b92df4c931e2e14
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-29 05:13:18 +0100

    userData.map

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ const userDisplay = async () => {
     (user) =>
     `
     <div class="card">
-      <img src=${user.picture.large} alt="photo de ${user.name.last}"
+      <img src=${user.picture.large} alt="photo de ${user.name.last}">
       <h3>${user.name.first}</h3>
+      <p>${user.location.city}, ${user.dob.date}</p>
+      <em>Membre depuis : ${user.registered.date} jours</em>
     </div>
     `
   ).join("");
