b9ad01348647faaf283862eb15964333dbb61e24
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-29 05:21:44 +0100

    dateParser

--- index.js
+++ index.js
@@ -16,13 +16,22 @@ const fetchUser = async () => {
 const userDisplay = async () => {
   await fetchUser();
 
+  const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return newDate;
+  };
+
   document.body.innerHTML = userData.map(
     (user) =>
     `
     <div class="card">
       <img src=${user.picture.large} alt="photo de ${user.name.last}">
       <h3>${user.name.first}</h3>
-      <p>${user.location.city}, ${user.dob.date}</p>
+      <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
       <em>Membre depuis : ${user.registered.date} jours</em>
     </div>
     `
