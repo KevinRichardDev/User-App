aaf58a21b5a87dbe2793f5dc1454a6998d5e1f66
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-30 05:03:29 +0100

    Fin du projet

--- index.js
+++ index.js
@@ -27,16 +27,19 @@ const userDisplay = async () => {
 
   const dayCalc = (date) => {
     let today = new Date();
+    let todayTimestamp = Date.parse(today);
+    let timestamp = Date.parse(date)
+    return Math.ceil((todayTimestamp - timestamp) / 8.64e7);
+  };
 
-    return today
-  }
+  // Pour arrondir au chiffre superieur Math.ceil()
 
   document.body.innerHTML = userData.map(
     (user) =>
     `
     <div class="card">
       <img src=${user.picture.large} alt="photo de ${user.name.last}">
-      <h3>${user.name.first}</h3>
+      <h3>${user.name.first} ${user.name.last}</h3>
       <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
       <em>Membre depuis : ${dayCalc(user.registered.date)} jours</em>
     </div>
